0be1ea4f15314f72b808900b10c3273b252b4012
vinograd-j <vinograd1020gm@gmail.com>
2025-11-20 22:36:54 +0300

    initial commit

---
+++ src/main/java/net/vinograd/enterprise/EnterprisePracticeApplication.java
@@ -0,0 +1,13 @@
+package net.vinograd.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterprisePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterprisePracticeApplication.class, args);
+    }
+
+}
